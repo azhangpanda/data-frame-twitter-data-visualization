e0b62a244521a23dd5a0a0007c09f3cc369a1508
azhangpanda <aiyizhang@gmail.com>
2014-12-16 14:12:10 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,12 +1,9 @@
 # server.R
 require(mosaic)
-#twitter=read.csv("data/eqTwitter")
 require(rJava)
 require(repmis)
-twitter=source_DropboxData(file = "eqTwitter", key = "qc0mmglrkqklfk5", sep = ",", header = TRUE, fileEncoding="latin1")
-#twitter=read.csv("~/Documents/academic/fall 14/data science/dataframe/data/eqTwitter",header=TRUE,fileEncoding="UCS-2LE")
+twitter=source_DropboxData(file = "eqTwitter", key = "qc0mmglrkqklfk5", sep = ",", header = TRUE, fileEncoding="UCS-2LE")
 twitter=(select(twitter, name, city, X.1))
-#names(twitter) <- c("Trend", "City", "Date")
 library(ggplot2)
 library(shiny)
 shinyServer(function(input, output) {
